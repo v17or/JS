f3327479a544532bc74846fe73e1b117eb8f4c73
v170r <vitor.sartori50@gmail.com>
2021-07-20 21:45:06 -0300

    same updates

---
+++ boleanos.js
@@ -0,0 +1,24 @@
+let isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo)
+
+console.log('os verdadeiros ...')
+console.log(!!3)
+console.log(!!-1)
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(isAtivo = Infinity))
+
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isAtivo = false))
+
---
+++ strings.js
@@ -0,0 +1,15 @@
+const escola ="Cod3r"
+
+console.log(escola.charAt(4))
+console.log(escola.charAt(5))
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf(3))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0, 4))
+
+console.log('Escola '.concat(escola).concat("!"))
+console.log(escola.replace(3, 'e'))
+
+console.log('Ana, Maria, Pedro'.split(','))
+console.log('Ana, Maria, e Pedro vão a escola '.concat(escola).concat("!"))
