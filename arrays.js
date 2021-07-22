cd03b662b0b6025a239611ac8446299d5ddfe92b
Vitor <vitor.sartori50@gmail.com>
2021-07-22 13:19:02 -0300

    Same updates!

--- arrays.js
+++ arrays.js
@@ -5,4 +5,12 @@ console.log(valores[4])
 valores [4] = 10
 console.log(valores)
 console.log(valores.length)
-valores.push()
+
+valores.push({id:3}, false, null, 'teste')
+console.log(valores)
+
+console.log(valores.pop())
+delete valores [0]
+console.log(valores)
+
+console.log(typeof valores)
