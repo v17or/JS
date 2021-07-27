dadf0d8e152941c7b172a4e18004cd862cbe1cc8
v170r <vitor.sartori50@gmail.com>
2021-07-26 21:32:36 -0300

    Updates VAR

---
+++ usandoVar1.js
@@ -0,0 +1,8 @@
+{{{{ var sera = 'Será???'}}}}
+console.log(sera) 
+function teste() {
+    var local = 123
+}
+
+teste()
+console.log(local)
---
+++ usandoVar2.js
@@ -0,0 +1,6 @@
+var number = 1
+{
+    var numero = 2
+    console.log('dentro =', numero)
+}
+console.log('fora =', numero)
