38d9ea5e39262f6d4693472df107090c26aede33
Vitor <vitor.sartori50@gmail.com>
2021-08-06 09:40:44 -0300

    Update

---
+++ destructuring1.js
@@ -0,0 +1,14 @@
+const pessoa = {
+    nome: 'nome',
+    idade: 5,
+    endereco: {
+        logradouro: 'Rua ABC',
+        numero: 1000
+    }
+}
+
+const { nome, idade } = pessoa //Se usado const com chaves irá extrair objetos
+console.log(nome, idade)
+
+const {endereco: {logradouro, numero}} = pessoa
+console.log(logradouro, numero, cep)
