fed2dfdc41b32251d82c738bb46b561ba183bf79
Vitor <vitor.sartori50@gmail.com>
2021-08-06 07:14:52 -0300

    up!

--- parNomeValor.js
+++ parNomeValor.js
@@ -4,3 +4,15 @@ function exec() {
     const saudacao = 'Falaaaa'
     return saudacao
 }
+const cliente = {
+    nome: 'Pedro',
+    idade: 32,
+    peso: 90,
+    endereco: {
+        logradouro: 'Rua Muito Legal',
+        numero: 123
+    }
+}
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
